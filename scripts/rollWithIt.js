f8881096072af1dfce52df04e39179ec44fd59ca
Marco Salsiccia <marco.salsiccia@gmail.com>
2025-12-19 11:04:45 -0800

    Changed reults text, starting point for structural and code cleanup phases.

--- scripts/rollWithIt.js
+++ scripts/rollWithIt.js
@@ -1,16 +1,5 @@
 const apiKey = "";
-let lyricsText = `We're no strangers to love
-You know the rules and so do I
-A full commitment's what I'm thinkin' of
-You wouldn't get this from any other guy
-I just wanna tell you how I'm feeling
-Gotta make you understand
-Never gonna give you up
-Never gonna let you down
-Never gonna run around and desert you
-Never gonna make you cry
-Never gonna say goodbye
-Never gonna tell a lie and hurt you`;
+let lyricsText = ``;
 
 let lines = [];
 let currentLineIndex = 0;
